1850a3f635e8407854d0fa0932eb3aacfe4bf892
ahmadsuherman <suhermana274@gmail.com>
2021-10-22 16:44:11 +0700

    add title name

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	$(window).on('scroll', function(){
 		var scroll = $(window).scrollTop();
-		console.log(scroll)
 		if (scroll >= 50) {
 			$(".sticky").addClass("stickyadd");
 		}else{
