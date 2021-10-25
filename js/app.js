3158e0d6a8c1ac61b45a98c24b1ad360f284bdd5
ahmadsuherman <suhermana274@gmail.com>
2021-10-25 17:19:10 +0700

    add movie feature

--- js/app.js
+++ js/app.js
@@ -34,4 +34,120 @@ $(document).ready(function(){
 	  offset: '90%'
 	})
 	
+	$('#search-text').on('keyup', function(e){
+	    if(e.keyCode == 13) //tombol enter
+	    { 
+	        searchMovies();
+	    }
+	})
+
+	$('#btn-search').on('click', function(e){
+	    searchMovies();
+	})
+
+	// Modals
+	$('#movie-list').on('click', '#btn-details', function(e){
+	    $.ajax({
+	        url:'http://www.omdbapi.com',
+	        type:'GET',
+	        dataType:'json',
+	        data:{
+	            'apikey' : '13415ac3',
+	            'i' : $(this).data('id') 
+	        },
+	        success: function(result){
+	            if(result.Response === "True"){
+	                $('.modal-body').html('');
+	                $('.modal-body').append(`
+	                    <div class="container-fluid">
+	                        <div class="row">
+	                            <div class="col-md-4 d-flex justify-content-center align-items-center">
+	                                <img src="` + result.Poster + `" class="img-fluid img-movie" alt="img-movie-detail">
+	                            </div>
+	                            <div class="col-md-8">
+	                                <ul class="list-group">
+	                                  <li class="list-group-item"><h4>`+ result.Title +`</h4></li>
+	                                  <li class="list-group-item">Dirilis : `+ result.Released +`</li>
+	                                  <li class="list-group-item">Aliran : `+ result.Genre +`</li>
+	                                  <li class="list-group-item">Penulis : `+ result.Writer +`</li>
+	                                  <li class="list-group-item">Aktor : `+ result.Actors +`</li>
+	                                  <li class="list-group-item">Produksi : `+ result.Production +`</li>
+	                                  <li class="list-group-item">Alur Cerita : `+ result.Plot +`</li>
+	                                </ul>
+	                            </div>
+	                        </div>
+	                    </div>    
+	                `);
+	            }
+	        }
+	    });
+	})
+
 })
+
+
+function searchMovies(){
+    $.ajax({
+        url:'http://www.omdbapi.com',
+        type:'GET',
+        dataType:'json',
+        data:{
+            'apikey' : '13415ac3', 
+            's' : $('#search-text').val() 
+        },
+        success: function(result){
+            let movies = result.Search;
+            $('#movie-list').html('');
+            if(result.Response == "True"){
+            	// console.log(result.totalResults)
+                $.each(movies, function(i, data){
+                    $('#movie-list').append(`<div class="col-lg-4 mb-4 card-movie">
+		                <div class="card h-100">
+							<div class="card-header">
+								<img src="`+ data.Poster +`" alt="img-movie" class="card-img-top img-movie" height="300px">
+							</div>
+
+							<div class="card-body">
+								<h5 class="card-title">`+ data.Title +`</h5>
+								<p class="card-text">Tahun: `+ data.Year +` </p>
+	                        	<a href="#" class="btn btn-primary btn-sm" id="btn-details" data-id="`+ data.imdbID +`" 
+                    			data-toggle="modal" data-target="#exampleModal"> Detail</a>
+							</div>
+						</div>
+	                </div>
+                    `);
+                })
+
+                var items = $('#movie-list .card-movie');
+                // console.log(items.length)
+	            var numItems = result.totalResults;
+	            var perPage = 10;
+
+	            items.slice(perPage).hide();
+
+	            $('#pagination-container').pagination({
+	                items: numItems,
+	                itemsOnPage: perPage,
+	                prevText:"<",
+	                nextText:">",
+	                onPageClick: function(pageNumber){
+	                    var showFrom = perPage *(pageNumber - 1);
+	                    var showTo = showFrom + perPage;
+	                    items.hide().slice(showFrom, showTo).show();
+	                }
+	            }).show()
+
+            }else{
+                $('#movie-list').append(`
+                    <div class="col-sm-12 text-center">
+                        <h1>`+ result.Error +`</h1>
+                    </div>
+                `);
+
+                $('#pagination-container').hide()
+            }
+        }
+    });
+
+    $('#search-text').val("");
+}
