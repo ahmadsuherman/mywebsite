8ae7eaf8cb0452b188bd7f68224e57ae75dacb4d
ahmadsuherman <suhermana274@gmail.com>
2021-10-28 17:21:37 +0700

    revision source code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$(window).on('scroll', function(){
-		var scroll = $(window).scrollTop();
+		const scroll = $(window).scrollTop();
 		if (scroll >= 50) {
 			$(".sticky").addClass("stickyadd");
 		}else{
@@ -8,47 +8,31 @@ $(document).ready(function(){
 		}
 	})
 
-	var typed = new Typed('.element', {
-		strings: ['Ahmad Suherman', 'a Developer', 'a Designer', 'a Businessman'],
-		smartBackspace: true,
-		typeSpeed: 100,
-		backSpeed: 100,
-		loop: true,
-		loopCount: Infinity,
-		startDelay: 1000
-	});
+	typed()
 
 	// Progress Bars
+	waypoint()
 
-	var waypoint = new Waypoint({
-	  element: document.getElementById('exp-id'),
-	  handler: function() {
-		var p = document.querySelectorAll('.progress-bar');
-		p[0].setAttribute('style', 'width: 100%; transition: 1s all');
-		p[1].setAttribute('style', 'width: 95%; transition: 1.5s all');
-		p[2].setAttribute('style', 'width: 85%; transition: 1.7s all');
-		p[3].setAttribute('style', 'width: 99%; transition: 2s all');
-		p[4].setAttribute('style', 'width: 85%; transition: 2.3s all');
-		p[5].setAttribute('style', 'width: 95%; transition: 2.5s all');
-	  },
-	  offset: '90%'
+	$('#btnSearchMovie').on('click', function(e){
+	    const search = $('#searchTextMovie').val()
+		,page = 1
+	    searchMovies(search, page);
 	})
 
-
-	$('#btn-search').on('click', function(e){
-	    searchMovies();
+	$('#btnSearchBook').on('click', function(e){
+		const search = $('#searchTextBook').val()
+	    , page = 1
+	    searchBooks(search, page);
 	})
 
-	$('#btn-search-book').on('click', function(e){
-	    searchBooks();
-	})
-
-	$('#btn-search-song').on('click', function(e){
+	$('#btnSearchSong').on('click', function(e){
 	    searchSongs();
 	})
 
 	// Modals
-	$('#movie-list').on('click', '#btn-details', function(e){
+	// $('#movie-list').modal('show')
+
+	$('#movieList').on('click', '#btnDetailMovie', function(e){
 	    $.ajax({
 	        url:'https://www.omdbapi.com',
 	        type:'GET',
@@ -59,7 +43,7 @@ $(document).ready(function(){
 	        },
 	        success: function(result){
 	            if(result.Response === "True"){
-	            	$('#titleModal').html("Movie Detail");
+	            	$('#titleModal').text("Movie Detail");
 	                $('.modal-body').html('');
 	                let production = result.Production ? result.Production : '-'
 	                $('.modal-body').append(`
@@ -83,18 +67,21 @@ $(document).ready(function(){
 	                    </div>    
 	                `);
 	            }
+	        },
+	        error: function (e) {
+	          	alert(e)
 	        }
 	    });
 	})
 
-	$('#book-list').on('click', '#btn-detail-books', function(e){
-		var id = $(this).data('id')
+	$('#bookList').on('click', '#btnDetailBooks', function(e){
+		const id = $(this).data('id')
 	    $.ajax({
 	        url:'https://www.googleapis.com/books/v1/volumes/' + id,
 	        type:'GET',
 	        dataType:'json',
 	        success: function(result){
-	        	$('#titleModal').html("Book Detail");
+	        	$('#titleModal').text("Book Detail");
                 $('.modal-body').html('');
                 let title = result.volumeInfo.title ? result.volumeInfo.title : '-',
                 	categories = result.volumeInfo.categories ? result.volumeInfo.categories.join(', ') : '-',
@@ -125,18 +112,21 @@ $(document).ready(function(){
                         </div>
                     </div>    
                 `);
+	        },
+	        error: function (e) {
+	          alert(e)
 	        }
 	    });
 	})
 
-	$('#song-list').on('click', '#btn-detail-songs', function(e){
-		var id = $(this).data('id')
+	$('#songList').on('click', '#btnDetailSongs', function(e){
+		const id = $(this).data('id')
 	    $.ajax({
-	        url:'https://api-song-lyrics.herokuapp.com/lyrics/' + id,
+	        url:'https://song-lyrics-api-o0m8tth8t-azharimm.vercel.app/lyrics/' + id,
 	        type:'GET',
 	        dataType:'json',
 	        success: function(result){
-	        	$('#titleModal').html("Song Lyrics Detail");
+	        	$('#titleModal').text("Song Lyrics Detail");
                 $('.modal-body').html('');
                 
                 $('.modal-body').append(`
@@ -154,14 +144,46 @@ $(document).ready(function(){
                         </div>
                     </div>    
                 `);
+	        },
+	        error: function (e) {
+	          	alert(e)
 	        }
 	    });
 	})
 	
 })
 
-function searchMovies(){
-	var search = $('#search-text').val()
+function typed()
+{
+	const typed = new Typed('.element', {
+		strings: ['Ahmad Suherman', 'a Developer', 'a Designer', 'a Businessman'],
+		smartBackspace: true,
+		typeSpeed: 100,
+		backSpeed: 100,
+		loop: true,
+		loopCount: Infinity,
+		startDelay: 1000
+	});
+}
+
+function waypoint()
+{
+	const waypoint = new Waypoint({
+	  element: document.getElementById('expId'),
+	  handler: function() {
+		const p = document.querySelectorAll('.progress-bar');
+		p[0].setAttribute('style', 'width: 100%; transition: 1s all');
+		p[1].setAttribute('style', 'width: 95%; transition: 1.5s all');
+		p[2].setAttribute('style', 'width: 85%; transition: 1.7s all');
+		p[3].setAttribute('style', 'width: 99%; transition: 2s all');
+		p[4].setAttribute('style', 'width: 85%; transition: 2.3s all');
+		p[5].setAttribute('style', 'width: 95%; transition: 2.5s all');
+	  },
+	  offset: '90%'
+	})
+}
+
+function searchMovies(search, page){
     $.ajax({
         url:'https://www.omdbapi.com',
         type:'GET',
@@ -169,14 +191,15 @@ function searchMovies(){
         data:{
             'apikey' : '13415ac3', 
             's' : search,
+            'page' : page
         },
         success: function(result){
             let movies = result.Search;
-            $('#movie-list').html('');
+            $('#movieList').html('');
             if(result.Response == "True"){
             	// console.log(result.totalResults)
                 $.each(movies, function(i, data){
-                    $('#movie-list').append(`<div class="col-lg-4 mb-4 card-movie">
+                    $('#movieList').append(`<div class="col-lg-4 mb-4 card-movie">
 		                <div class="card h-100">
 							<div class="card-header">
 								<img src="`+ data.Poster +`" alt="img-movie" class="card-img-top img-movie" height="300px">
@@ -185,7 +208,7 @@ function searchMovies(){
 							<div class="card-body">
 								<h5 class="card-title">`+ data.Title +`</h5>
 								<p class="card-text">Year: `+ data.Year +` </p>
-	                        	<a href="#" class="btn btn-primary btn-sm" id="btn-details" data-id="`+ data.imdbID +`" 
+	                        	<a href="#" class="btn btn-primary btn-sm btn-movie" id="btnDetailMovie" data-id="`+ data.imdbID +`" 
                     			data-toggle="modal" data-target="#exampleModal"> Detail</a>
 							</div>
 						</div>
@@ -193,55 +216,55 @@ function searchMovies(){
                     `);
                 })
 
-                var items = $('#movie-list .card-movie');
-                // console.log(items.length)
-	            var numItems = result.totalResults;
-	            var perPage = 10;
+                if (page === 1) {
+                	const items = $('#movieList .card-movie')
+		            , numItems = result.totalResults
+		            , perPage = 10
 
-	            items.slice(perPage).hide();
-
-	            $('#pagination-container').pagination({
-	                items: numItems,
-	                itemsOnPage: perPage,
-	                prevText:"<",
-	                nextText:">",
-	                onPageClick: function(pageNumber){
-	                    showDataMovieAfterPagination(pageNumber, search)
-	                }
-	            }).show()
+		            $('#paginationContainerMovie').pagination({
+		                items: numItems,
+		                itemsOnPage: perPage,
+		                prevText:"&lt;",
+		                nextText:"&gt;",
+		                onPageClick: function(pageNumber){
+		                    searchMovies(search, pageNumber)
+		                }
+		            }).show()
+                }
 
             }else{
-                $('#movie-list').append(`
+                $('#movieList').append(`
                     <div class="col-sm-12 text-center">
                         <h1>`+ result.Error +`</h1>
                     </div>
                 `);
-
-                $('#pagination-container').hide()
+                $('#paginationContainerMovie').hide()
             }
+        },
+        error: function (e) {
+          alert(e)
         }
     });
 }
 
 
-function searchBooks(){
-	var search = $('#search-text-book').val()
-	// console.log(search)
+function searchBooks(search, page){
     $.ajax({
         url:'https://www.googleapis.com/books/v1/volumes',
         type:'GET',
         dataType:'json',
         data:{
             'q' : search,
+            'page' : page
         },
         success: function(result){
-            $('#book-list').html('');
+            $('#bookList').html('');
             if(result.items){
             	let books = result.items;
                 $.each(books, function(i, data){
                 	let thumbnail = data.volumeInfo.imageLinks ? data.volumeInfo.imageLinks.thumbnail : '-',
                 		title = data.volumeInfo.title ? data.volumeInfo.title : '-'
-                    $('#book-list').append(`<div class="col-lg-4 mb-4 card-book">
+                    $('#bookList').append(`<div class="col-lg-4 mb-4 card-book">
 		                <div class="card h-100">
 							<div class="card-header">
 								<img src="`+ thumbnail +`" alt="img-book" class="card-img-top img-movie" height="300px">
@@ -249,7 +272,7 @@ function searchBooks(){
 
 							<div class="card-body">
 								<h5 class="card-title">`+ title +`</h5>
-	                        	<a href="#" class="btn btn-primary btn-sm" id="btn-detail-books" data-id="`+ data.id +`" 
+	                        	<a href="#" class="btn btn-primary btn-sm" id="btnDetailBooks" data-id="`+ data.id +`" 
                     			data-toggle="modal" data-target="#exampleModal"> Detail</a>
 							</div>
 						</div>
@@ -257,57 +280,65 @@ function searchBooks(){
                     `);
                 })
 
-                var items = $('#book-list .card-book');
-	            var numItems = result.totalItems;
-	            var perPage = 10;
-	            items.slice(perPage).hide();
+               if (page === 1) {
+               	 	const items = $('#bookList .card-book')
+		            , numItems = result.totalItems
+		            , perPage = 10
 
-	            $('#pagination-container-book').pagination({
-	                items: numItems,
-	                itemsOnPage: perPage,
-	                prevText:"<",
-	                nextText:">",
-	                onPageClick: function(pageNumber){
-	                    showDataBookAfterPagination(pageNumber, search)
-	                }
-	            }).show()
+		            $('#paginationContainerBook').pagination({
+		                items: numItems,
+		                itemsOnPage: perPage,
+		                prevText:"&lt;",
+		                nextText:"&gt;",
+		                onPageClick: function(pageNumber){
+		                    searchBooks(pageNumber, search)
+		                }
+		            })
+               }
 
             }else{
-                $('#book-list').append(`
+                $('#bookList').append(`
                     <div class="col-sm-12 text-center">
                         <h1> Buku not found! </h1>
                     </div>
                 `);
 
-                $('#pagination-container-book').hide()
+                $('#paginationContainerBook').hide()
             }
+        },
+        error: function (e) {
+        	$('#bookList').html('');
+           	$('#bookList').append(`
+                <div class="col-sm-12 text-center">
+                    <h1> `+ e.responseJSON.error.message +` </h1>
+                </div>
+            `);
+
+            $('#paginationContainerBook').hide()
         }
     });
 }
 
 function searchSongs(){
-	var search = $('#search-text-song').val()
-	// console.log(search)
+	const search = $('#searchTextSong').val()
     $.ajax({
-        url:'https://api-song-lyrics.herokuapp.com/search',
+        url:'https://song-lyrics-api-o0m8tth8t-azharimm.vercel.app/search',
         type:'GET',
         dataType:'json',
         data:{
             'q' : search,
         },
         success: function(result){
-            $('#song-list').html('');
+            $('#songList').html('');
             if(result.data.length !== 0){
             	let song = result.data;
                 $.each(song, function(i, data){
-                	// let thumbnail = data.volumeInfo.imageLinks ? data.volumeInfo.imageLinks.thumbnail : '-',
-                	// 	title = data.volumeInfo.title ? data.volumeInfo.title : '-'
-                    $('#song-list').append(`<div class="col-lg-4 mb-4 card-song">
+                    $('#songList').append(`<div class="col-lg-4 mb-4 card-song">
 		                <div class="card h-100">
 							<div class="card-body">
 								<h5 class="card-title">`+ data.songTitle +`</h5>
 								<p class="card-text">Artist: `+ data.artist +` </p>
-	                        	<a href="#" class="btn btn-primary btn-sm" id="btn-detail-songs" data-id="`+ data.songId +`" 
+	                        	<a href="#" class="btn btn-primary btn-sm" id="btnDetailSongs" data-id="`+ data.songId +`" 
                     			data-toggle="modal" data-target="#exampleModal"> Detail</a>
 							</div>
 						</div>
@@ -315,98 +346,42 @@ function searchSongs(){
                     `);
                 })
 
-                var items = $('#song-list .card-song');
-	            var numItems = result.data.length;
-	            var perPage = 10;
+                const items = $('#songList .card-song')
+	            , numItems = result.data.length
+	            , perPage = 10
 	            items.slice(perPage).hide();
 
-	            $('#pagination-container-song').pagination({
+	            $('#paginationContainerSong').pagination({
 	                items: numItems,
 	                itemsOnPage: perPage,
-	                prevText:"<",
-	                nextText:">",
+	                prevText:"&lt;",
+		            nextText:"&gt;",
 	                onPageClick: function(pageNumber){
-	                	var showFrom = perPage *(pageNumber - 1);
-                    	var showTo = showFrom + perPage;
+	                	const showFrom = perPage *(pageNumber - 1)
+                    	, showTo = showFrom + perPage
                     	items.hide().slice(showFrom, showTo).show();
 	                }
-	            }).show()
+	            })
 
             }else{
-                $('#song-list').append(`
+                $('#songList').append(`
                     <div class="col-sm-12 text-center">
                         <h1> Song Lyrics not found! </h1>
                     </div>
                 `);
 
-                $('#pagination-container-song').hide()
+                $('#paginationContainerSong').hide()
             }
-        }
-    });
-}
-
-function showDataMovieAfterPagination(page, search){
-    $.ajax({
-        url:'https://www.omdbapi.com',
-        type:'GET',
-        dataType:'json',
-        data:{
-        	'apikey' : '13415ac3', 
-            's' : search,
-            'page': page
         },
-        success: function(result){
-            let movies = result.Search;
-            $('#movie-list').html('');
-            	$.each(movies, function(i, data){
-                    $('#movie-list').append(`<div class="col-lg-4 mb-4 card-movie">
-		                <div class="card h-100">
-							<div class="card-header">
-								<img src="`+ data.Poster +`" alt="img-movie" class="card-img-top img-movie" height="300px">
-							</div>
+        error: function (e) {
+        	$('#songList').html('');
+          	$('#songList').append(`
+	            <div class="col-sm-12 text-center">
+	                <h1> Something went wrong: Please fill in the search query! </h1>
+	            </div>
+	        `);
 
-							<div class="card-body">
-								<h5 class="card-title">`+ data.Title +`</h5>
-								<p class="card-text">Year: `+ data.Year +` </p>
-	                        	<a href="#" class="btn btn-primary btn-sm" id="btn-details" data-id="`+ data.imdbID +`" 
-                    			data-toggle="modal" data-target="#exampleModal"> Detail</a>
-							</div>
-						</div>
-	                </div>
-                    `);
-                })
-        	}
-    });
-}
-
-function showDataBookAfterPagination(page, search){
-    $.ajax({
-        url:'https://www.googleapis.com/books/v1/volumes?q='+ search +'?page=' + page,
-        type:'GET',
-        dataType:'json',
-
-        success: function(result){
-            let books = result.items;
-            $('#book-list').html('');
-            $.each(books, function(i, data){
-                let thumbnail = data.volumeInfo.imageLinks ? data.volumeInfo.imageLinks.thumbnail : '-',
-                		title = data.volumeInfo.title ? data.volumeInfo.title : '-'
-                		
-                    $('#book-list').append(`<div class="col-lg-4 mb-4 card-book">
-		                <div class="card h-100">
-							<div class="card-header">
-								<img src="`+ thumbnail +`" alt="img-book" class="card-img-top img-movie" height="300px">
-							</div>
-
-							<div class="card-body">
-								<h5 class="card-title">`+ title +`</h5>
-	                        	<a href="#" class="btn btn-primary btn-sm" id="btn-detail-books" data-id="`+ data.id +`" 
-                    			data-toggle="modal" data-target="#exampleModal"> Detail</a>
-							</div>
-						</div>
-	                </div>
-                `);
-            })
+	        $('#paginationContainerSong').hide()
         }
     });
 }
